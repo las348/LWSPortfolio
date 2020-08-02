fe6c60db08beb03ceb4f67e4bedc8e7f878ba45d
Laureni Wilkinson <las348@nyu.edu>
2020-08-02 19:09:37 -0400

    Updates

---
+++ precache-manifest.3acb8c75b300f167ec320c5967367acb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ec7275c199f9d47b7e5d45ce87f824",
+    "url": "/LWSPortfolio/index.html"
+  },
+  {
+    "revision": "a536f85f989007b9e6cf",
+    "url": "/LWSPortfolio/static/css/main.5fa8412c.chunk.css"
+  },
+  {
+    "revision": "482650c107ef984cee28",
+    "url": "/LWSPortfolio/static/js/2.535be83f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/LWSPortfolio/static/js/2.535be83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a536f85f989007b9e6cf",
+    "url": "/LWSPortfolio/static/js/main.374ff4e6.chunk.js"
+  },
+  {
+    "revision": "7d9877e4a21593e7c367",
+    "url": "/LWSPortfolio/static/js/runtime-main.f6629c55.js"
+  },
+  {
+    "revision": "a4ca2732528d9a78ef3848c54fc33d72",
+    "url": "/LWSPortfolio/static/media/Planner.a4ca2732.PNG"
+  },
+  {
+    "revision": "c62ed4547403545b271a67b8637e6288",
+    "url": "/LWSPortfolio/static/media/Weather.c62ed454.PNG"
+  },
+  {
+    "revision": "2b44f8ada8d7a38d500ae89dd2ce3273",
+    "url": "/LWSPortfolio/static/media/background.2b44f8ad.jpg"
+  },
+  {
+    "revision": "9768afb2d633801d79ec8fbd61eed32d",
+    "url": "/LWSPortfolio/static/media/burger.9768afb2.PNG"
+  },
+  {
+    "revision": "06c7f34a03cca331dea3a8930055f631",
+    "url": "/LWSPortfolio/static/media/laureni.06c7f34a.jpg"
+  },
+  {
+    "revision": "dc645f79a9ab6d64d446133cf82e66f3",
+    "url": "/LWSPortfolio/static/media/movie.dc645f79.png"
+  },
+  {
+    "revision": "526db0b7650eca4cf8464cfee035de04",
+    "url": "/LWSPortfolio/static/media/nyu.526db0b7.png"
+  },
+  {
+    "revision": "fa82e379d01e4749271391b5491e3753",
+    "url": "/LWSPortfolio/static/media/recipes.fa82e379.PNG"
+  },
+  {
+    "revision": "f991e1d6b0d0150af197a03074aa8e4a",
+    "url": "/LWSPortfolio/static/media/rutgers.f991e1d6.png"
+  },
+  {
+    "revision": "5a2f4fbf045a39d4ea11ffc7a1c7adc7",
+    "url": "/LWSPortfolio/static/media/trivia.5a2f4fbf.PNG"
+  }
+]);
